e2f19364e7dd0697245cb12931af3adac7d51df8
waffles42 <ivan_z@live.com>
2021-07-27 14:33:04 +0200

    update

--- app.js
+++ app.js
@@ -26,38 +26,38 @@ async function sendHttpRequest(method, url, data) {
     });
 }
 
-// let loc = document.getElementById("imageList");
-//
-// function createDiv() {
-//   var div = document.createElement("div");
-//   div.style.width = "100px";
-//   div.style.height = "100px";
-//   div.style.background = "red";
-//   div.style.color = "white";
-//   div.innerHTML = "Hello";
-//
-//   loc.appendChild(div);
-// }
+//  <form>
+//      <input id="myInput" placeholder="Some text.." value="">
+//      <input type="submit" id="myBtn" value="Submit">
+//  </form>
+
+var input = document.getElementById("searchField");
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    getImages();
+  }
+});
 
 //request s autorizacijskim parametrom: "https://api.unsplash.com/search/photos?query=dogs&cliend_id=GlxGxboGXijJhZZKPi-A76KH5XUkD1d3XHCwfGOmbKc"
 async function getImages() {
   try {
-    const responseData = await sendHttpRequest(
-      "GET",
-      "https://api.unsplash.com/search/photos?query=dogs"
-    );
+    //dobije input iz polja, no aktivira se samo na pritisak gumba
+    const userInput = document.getElementById("searchField").value;
+    console.log(userInput);
+
+    const method = "GET";
+    const url = `https://api.unsplash.com/search/photos?query=${userInput}`;
+    const responseData = await sendHttpRequest(method, url);
+
     //console.log("Objekt responseData", responseData);
     //console.log("Array resulta", responseData.results);
     responseData.results.forEach((res) => {
-      console.log(res.urls.raw);
-      article.insertAdjacentHTML(
-        "afterend",
-        `<div class="images"><img src="${res.urls.thumb}"/></div>`
-      );
-      //dodati funkciju koja kreira sliku
+      //console.log(res.urls.raw);
+      const method = "afterend";
+      const url = `<div class="images"><img src="${res.urls.thumb}"/></div>`;
+      article.insertAdjacentHTML(method, url);
     });
-    //console.log(responseData);
-    // createDiv();
 
     return responseData;
   } catch (error) {
@@ -75,6 +75,5 @@ async function getImages() {
 // dynamically display data ✓
 // responsive design ✓
 // better looking display ✓
-
-// replace BUTTON with INPUT_FIELD
-// enable any input
+// replace BUTTON with INPUT_FIELD ✓
+// enable any input ✓
